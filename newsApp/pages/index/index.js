04000901b68d94d70deb548614019a844e273688
York Guan <yiranguan@hotmail.com>
2018-04-18 23:44:00 +0800

    featrue: Add news type nav and news type picture

--- newsApp/pages/index/index.js
+++ newsApp/pages/index/index.js
@@ -1,1 +1,55 @@
-Page({})
+Page({
+  data:{
+    newsType:[
+      {
+        text: '国内',
+        type: 'gn',
+        select: 'selected',
+        selectLine: 'selected-line'
+      }, 
+      {
+        text: '国际',
+        type: 'gj',
+        select: '',
+        selectLine: ''
+      },
+      {
+        text: '财经',
+        type: 'cj',
+        select: '',
+        selectLine: ''
+      },
+      {
+        text: '娱乐',
+        type: 'yl',
+        select: '',
+        selectLine: ''
+      },
+      {
+        text: '军事',
+        type: 'js',
+        select: '',
+        selectLine: ''
+      },
+      {
+        text: '体育',
+        type: 'ty',
+        select: '',
+        selectLine: ''
+      },
+      {
+        text: '其他',
+        type: 'other',
+        select: '',
+        selectLine: ''
+      }
+    ],
+    newTypePicURL: '/images/gn-pic.jpg'
+  },
+
+  onNewsTypeTap: event => {
+
+  },
+
+
+})
