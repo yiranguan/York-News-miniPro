c3097d1d871707727506bbc4ae1c22d035787da3
York Guan <yiranguan@hotmail.com>
2018-04-19 01:55:10 +0800

    feature: Finsh the index style #1

--- newsApp/pages/index/index.js
+++ newsApp/pages/index/index.js
@@ -44,7 +44,37 @@ Page({
         selectLine: ''
       }
     ],
-    newTypePicURL: '/images/gn-pic.jpg'
+    newTypePicURL: '/images/gn-pic.jpg',
+    newsList: [
+      {
+      "id": 1519631218506,
+      "title": "外媒称香港回归15年打破“经济将死”预言,尝试多行文本溢出，尝试多行文本溢出，尝试多行文本溢出，",
+      "date": "2012-07-01",
+      "source": "中国新闻网",
+      "firstImage": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
+      },
+      {
+        "id": 1519631218506,
+        "title": "外媒称香港回归15年打破“经济将死”预言",
+        "date": "2012-07-01",
+        "source": "中国新闻网",
+        "firstImage": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
+      },
+      {
+        "id": 1519631218506,
+        "title": "外媒称香港回归15年打破“经济将死”预言",
+        "date": "2012-07-01",
+        "source": "中国新闻网",
+        "firstImage": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
+      },
+      {
+        "id": 1519631218506,
+        "title": "外媒称香港回归15年打破“经济将死”预言",
+        "date": "2012-07-01",
+        "source": "中国新闻网",
+        "firstImage": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
+      },
+    ],
   },
 
   onNewsTypeTap: event => {
