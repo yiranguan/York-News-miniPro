234e7222e4a53a421c75c12619cf13f7b74781e2
York Guan <yiranguan@hotmail.com>
2018-04-20 00:35:17 +0800

    feature: Click news type change the news list. But try animation fail

--- newsApp/pages/index/index.js
+++ newsApp/pages/index/index.js
@@ -12,86 +12,75 @@ Page({
         text: '国内',
         type: 'gn',
         select: 'selected',
-        selectLine: 'selected-line'
+        selectLine: 'selected-line',
+        code:0
       }, 
       {
         text: '国际',
         type: 'gj',
         select: '',
-        selectLine: ''
+        selectLine: '',
+        code: 1
       },
       {
         text: '财经',
         type: 'cj',
         select: '',
-        selectLine: ''
+        selectLine: '',
+        code: 2
       },
       {
         text: '娱乐',
         type: 'yl',
         select: '',
-        selectLine: ''
+        selectLine: '',
+        code: 3
       },
       {
         text: '军事',
         type: 'js',
         select: '',
-        selectLine: ''
+        selectLine: '',
+        code: 4
       },
       {
         text: '体育',
         type: 'ty',
         select: '',
-        selectLine: ''
+        selectLine: '',
+        code: 5
       },
       {
         text: '其他',
         type: 'other',
         select: '',
-        selectLine: ''
+        selectLine: '',
+        code: 6
       }
     ],
-
     newTypePicURL: '/images/gn-pic.jpg',
-
-    newsList: [
-      {
-      "id": 1519631218506,
-      "title": "外媒称香港回归15年打破“经济将死”预言,尝试多行文本溢出，尝试多行文本溢出，尝试多行文本溢出，",
-      "date": "2012-07-01",
-      "source": "中国新闻网",
-      "firstImage": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
-      },
-      {
-        "id": 1519631218506,
-        "title": "外媒称香港回归15年打破“经济将死”预言",
-        "date": "2012-07-01",
-        "source": "中国新闻网",
-        "firstImage": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
-      },
-      {
-        "id": 1519631218506,
-        "title": "外媒称香港回归15年打破“经济将死”预言",
-        "date": "2012-07-01",
-        "source": "中国新闻网",
-        "firstImage": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
-      },
-      {
-        "id": 1519631218506,
-        "title": "外媒称香港回归15年打破“经济将死”预言",
-        "date": "2012-07-01",
-        "source": "中国新闻网",
-        "firstImage": "http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg"
-      },
-    ],
+    newsList: [],
+    animationData:{}
   },
 
-  onLoad(){
+  onLoad() {
     this.getList(TYPEINDEX, LISTINDEX)
   },
 
+  onReady() {
+    this.animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: "ease",
+    })
+  },
+  // 下方方法：点击分类，实现分类文字、标题图片的变化，以及不同列表的渲染
   onNewsTypeTap(event){
-
+    console.log(event)
+    LISTINDEX = 7
+    TYPEINDEX = event.currentTarget.dataset.type
+    this.tapNewsTypeAnimation()
+    this.setNewsTypeStyle()
+    this.getList(TYPEINDEX, LISTINDEX)
   },
 
   onNewstap(event){
@@ -117,26 +106,81 @@ Page({
   onReachBottom(){
 
   },
-
-  getList(typeIndex, listIndex){
+  // 下方getList()方法用来获取数据并设置data
+  getList(typeIndex, listIndex, callback){
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
       data: { 'type': this.data.newsType[typeIndex].type },
       success:res => {
         let result = res.data.result
-        let newsList = [] 
-        console.log('newsListDataIs: ', result)
-        for (let i=0; i<listIndex; i++) {
-          newsList.push(
-            result[i]
-          )
-        }
-        console.log('The newList will rending is :', newsList)
-        this.setData({
-          newsList: newsList
+        this.setNewsListData(result, listIndex)
+      },
+      fail:res => {
+        wx.showToast({
+          title: '尴尬了，页面加载失败',
         })
       },
+      complete:() => {
+        console.log('Callback is: ', callback)
+        callback && callback()
+      }
     })
+  },
+  // 下方setNewsListData(result, listIndex)方法，用来设置data，渲染页面
+  setNewsListData(result, listIndex){
+    let newsList = []
+    let listLength = 0
+    if(listIndex > result.length){
+      listLength = result.length
+    }else{
+      listLength = listIndex
+    } 
+    for (let i = 0; i < listLength; i++) {
+      newsList.push(
+        result[i]
+      )
+    }
+    this.setData({
+      newsList: newsList
+    })
+  },
+  // 下方方法实现分类文字效果、标题图片的变化
+  setNewsTypeStyle(){
+    let newsType = []
+    for (let i = 0; i < 7; i++) {
+      let content = this.data.newsType[i]
+      content.select = ''
+      content.selectLine = ''
+      newsType.push(content)
+    }
+    newsType[TYPEINDEX].select = 'selected'
+    newsType[TYPEINDEX].selectLine = 'selected-line'
+    this.setData({
+      newsType: newsType,
+      newTypePicURL: '/images/' + newsType[TYPEINDEX].type + '-pic.jpg'
+    })
+  },
+
+  tapNewsTypeAnimation(){
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
+
+    this.animation = animation
+
+    animation.scale(2, 2).rotate(45).step()
+    
+    this.setData({
+      animationData: this.animation.export()
+    })
+
+    setTimeout(function () {
+      animation.translate(30).step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 1000)
   }
 
 })
