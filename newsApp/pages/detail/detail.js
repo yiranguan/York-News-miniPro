b3f0a27373e0353679d07bce58d20e360531c684
York Guan <yiranguan@hotmail.com>
2018-04-20 23:43:01 +0800

    feature: Add all wxml tag and the page can show real data #2

--- newsApp/pages/detail/detail.js
+++ newsApp/pages/detail/detail.js
@@ -2,6 +2,7 @@ Page({
   data: {
     newsId:0,
     newsType:'',
+    detailData:{}
   },
   // 页面读取时，获得新闻ID和所属归类数据
   onLoad(option){
@@ -19,6 +20,11 @@ Page({
       success: res => {
         let result = res.data.result
         console.log('The detail API data is: ', result)
+        result.date = result.date.slice(0, 10) + ' ' + result.date.slice(11, 16)
+        
+        this.setData({
+          detailData:result
+        })
       },
       fail: res => {
         wx.showToast({
