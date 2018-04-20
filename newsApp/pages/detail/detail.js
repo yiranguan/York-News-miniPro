bd7a2c9d4ddd4e9dd88a8d4a9c9f1561352965e3
York Guan <yiranguan@hotmail.com>
2018-04-21 01:05:38 +0800

    feature: When page on ready get the id list #3

--- newsApp/pages/detail/detail.js
+++ newsApp/pages/detail/detail.js
@@ -2,7 +2,8 @@ Page({
   data: {
     newsId:0,
     newsType:'',
-    detailData:{}
+    detailData:{},
+    newsIdList:[],
   },
   // 页面读取时，获得新闻ID和所属归类数据
   onLoad(option){
@@ -12,14 +13,36 @@ Page({
     })
     this.getDetail()
   },
-
+  // onLoad（）加载完成后，获取该新闻分类内每条新闻的ID，发送到this.data.newsIdList
+  onReady(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: { 'type': this.data.newsType },
+      success: res => {
+        let result = res.data.result
+        let newsIdList = []
+        let len = result.length
+        for(let i=0; i<len; i++){
+          newsIdList.push(result[i].id)
+        }
+        this.setData({
+          newsIdList:newsIdList
+        })
+      },
+      fail: res => {
+        wx.showToast({
+          title: '尴尬了，页面加载失败',
+        })
+      }
+    })
+  },
+  // 获取新闻详情数据
   getDetail(callback) {
     wx.request({
       url: 'https://test-miniprogram.com/api/news/detail',
       data: { 'id': this.data.newsId },
       success: res => {
         let result = res.data.result
-        console.log('The detail API data is: ', result)
         result.date = result.date.slice(0, 10) + ' ' + result.date.slice(11, 16)
         
         this.setData({
